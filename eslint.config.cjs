d94b606d4398251c6412a3d9dd47b149e27d6f72
John Lam <johnvinhlam@me.com>
2025-07-31 22:07:04 -0700

    fix: eslint upgrade

---
+++ eslint.config.cjs
@@ -0,0 +1,33 @@
+const prettier = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    ignores: ['dist/']
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        global: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly'
+      }
+    },
+    plugins: {
+      prettier
+    },
+    rules: {
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error'
+    }
+  }
+]; 
